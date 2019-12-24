32d1ed2cc8a076fc7e79260bd9abef4190466896
Basil Latif <basillatif@gmail.com>
2019-12-24 10:14:15 -0800

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-#Variables that contain the user credentials to access Twitter API
-ACCESS_TOKEN = "25427892-H21lJF3nCqsgaQqhdKdxbREIAPKcnqXzhgFwwEiA2"
-ACCESS_TOKEN_SECRET = "o0yYSOe9Zpfe4SxnXVE0hKl0QWRDnF6H2zf0P8Rv8vsEu"
-CONSUMER_KEY = "bvMXW33PQ0E7gQDupq3HyvReF"
-CONSUMER_SECRET = "z490x6gveZizniAyeoiuUFJZBzH7iZHCE6HLabma2iZGdhq9W6"
